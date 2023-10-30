d81520036ccda2624bb0b51b458a6b2056996dd7
test user <test@yopmail.com>
2023-10-30 17:08:43 +0530

    first commit

---
+++ First_Project/src/test/java/TestCases/TestClass.java
@@ -0,0 +1,23 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+
+	}
+	@Test
+	public void test2() {
+		System.out.println("Test 2");
+
+	}
+	@Test
+	public void test3() {
+		System.out.println("Test 3");
+
+	}
+	
+
+}
